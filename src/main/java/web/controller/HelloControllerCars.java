fe1906253fedaf2cff1ad50bbf5b466fe3aa34c3
Evgeny <vasilkalin@gmail.com>
2021-06-27 17:59:41 +0200

    Don't show cars in table

--- src/main/java/web/controller/HelloControllerCars.java
+++ src/main/java/web/controller/HelloControllerCars.java
@@ -1,32 +1,28 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 @Controller
 public class HelloControllerCars {
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("I'm controller Cars");
 
-        model.addAttribute("messages", messages);
-        return "index";
+    public String viewCars(Model model) {
+        //int count = Integer.parseInt(request.getParameter("count"));
+        //String[] cars = new String[] {"car1", "car2", "car3", "car4", "car5"};
+        //model.addAttribute("cars", cars[count]);
+        model.addAttribute("listCar", new CarService().getlistCars());
+        return "cars";
     }
-
-    List<Car> listCar = new ArrayList();
-    Car Car1 = new Car("Tesla", "5", "190");
-    Car Car2 = new Car("Tesla", "6", "200");
-    Car Car3 = new Car("Tesla", "7", "210");
-    Car Car4 = new Car("BMW", "8", "220");
-    Car Car5 = new Car("Toyota", "9", "230");
-
-
-
 
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -2,11 +2,25 @@ package web.service;
 
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarService {
-    private Car listCar;
-    Car getlistCars() {
+    private List<Car> listCar = new ArrayList<>();
+
+    public List<Car> getlistCars() {
+        Car car1 = new Car("Tesla", "5", "190");
+        Car car2 = new Car("Tesla", "6", "200");
+        Car car3 = new Car("Tesla", "7", "210");
+        Car car4 = new Car("BMW", "8", "220");
+        Car car5 = new Car("Toyota", "9", "230");
+
+        listCar.add(car1);
+        listCar.add(car2);
+        listCar.add(car3);
+        listCar.add(car4);
+        listCar.add(car5);
+
         return listCar;
     }
 }
