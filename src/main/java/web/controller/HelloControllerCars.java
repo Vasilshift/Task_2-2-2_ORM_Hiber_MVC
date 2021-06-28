6f17074f85371f80b1bddd25888c8dda8fe1c12e
Evgeny <vasilkalin@gmail.com>
2021-06-28 21:59:32 +0200

    Work variant. Need to formatting...

--- src/main/java/web/controller/HelloControllerCars.java
+++ src/main/java/web/controller/HelloControllerCars.java
@@ -2,34 +2,25 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 @Controller
-//@RequestMapping(value = "/cars")
 public class HelloControllerCars {
 
     @GetMapping("/cars")
     public String viewCars(Model model, @RequestParam(value = "count", required = false, defaultValue = "4") int count) {
         List<Car> listCars = new CarService().getlistCars();
-        //listCars.add(new Car("hjvj", "hvgj", "gchfgt"));
+        List<Car> listCarsNumbers = (List<Car>) new CarService().getlistCars().stream().limit(count).collect(Collectors.toList());
         model.addAttribute("listCars", listCars);
         System.out.println(listCars);
-        //String[] cars = new String[] {"car1", "car2", "car3", "car4", "car5"};
-        //model.addAttribute("cars", cars[count]);
-        //System.out.println(cars[count]);
 
+        model.addAttribute("listCarsNumbers", listCarsNumbers);
 
         return "cars";
     }
-
 }
